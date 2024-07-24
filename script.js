67591a0154c265cd89ada945234464fd7c09114e
JudahDevelops <judahdevelops@gmail.com>
2024-07-24 18:05:44 +0100

    About to use AI code backging up

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const weightValue = document.querySelector("#weight");
 const heightValue = document.querySelector("#height");
+const button = document.querySelector("button");
+const result =  document.querySelector("#bmi");
 const weight = weightValue.value;
 const height = heightValue.value;
-console.log(weight, height);
+
+
+function calculatebmi(weight, height) {
+    const finalBmi = (weight/height**2);
+   result.innerText = finalBmi;
+}
+
+button.onclick(bmiCalculator(weight, height));
