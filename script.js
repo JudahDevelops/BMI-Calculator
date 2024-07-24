2a66f94e0bbf36b87f66dcd71c2d3f5974687caa
JudahDevelops <judahdevelops@gmail.com>
2024-07-24 17:57:37 +0100

    Deleted teh result page and started javascript

---
+++ script.js
@@ -0,0 +1,5 @@
+const weightValue = document.querySelector("#weight");
+const heightValue = document.querySelector("#height");
+const weight = weightValue.value;
+const height = heightValue.value;
+console.log(weight, height);
