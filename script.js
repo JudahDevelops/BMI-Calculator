b1f7b778343eed4bb799a18046e7937dcf8d6850
JudahDevelops <judahdevelops@gmail.com>
2024-07-24 18:22:15 +0100

    Project successful

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const weightValue = document.querySelector("#weight");
 const heightValue = document.querySelector("#height");
-const button = document.querySelector("button");
-const result =  document.querySelector("#bmi");
-const weight = weightValue.value;
-const height = heightValue.value;
-
+const button = document.querySelector("#calculate");
+const result = document.querySelector("#bmi");
+const reset = document.querySelector("#reset");
 
 function calculatebmi(weight, height) {
-    const finalBmi = (weight/height**2);
-   result.innerText = finalBmi;
-}
+    const finalBmi = weight / (height ** 2);
+    result.innerText = finalBmi;
+  }
 
-button.onclick(bmiCalculator(weight, height));
+button.addEventListener("click", () => {
+  if (weightValue.value === "" || heightValue.value === "") {
+   result.innerText = "Please enter valid values";
+  } else {
+    const weight = parseFloat(weightValue.value);
+    const height = parseFloat(heightValue.value);
+    calculatebmi(weight, height);
+    reset.style.display = "block";
+  }
+});
+
+reset.addEventListener(
+    "click", () => {
+        weightValue.value = "";
+        heightValue.value = "";
+        result.innerText = "";
+        reset.style.display = "none";
+    }
+)
+
